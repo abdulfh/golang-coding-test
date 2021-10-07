2410f15bcc28021926d79e1397daf4685033342b
abdul.fatah <abdul.fatah@ottodigital.id>
2021-10-07 12:20:58 +0700

    Initial Router

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"idn/router"
+	"idn/utils/helper"
+)
+
 func main() {
-	
+	listenAddress := helper.GetEnv("SERVER_PORT", ":40001")
+	fmt.Println("Starting listen address: ", listenAddress)
+	router.Server(listenAddress)
 }
